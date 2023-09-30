37639bb029e249f2ecd26a0ab0fbfca5cdd01444
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-30 23:09:04 +0300

    Sorunla Yarın İlgileneceğim Lütfen Sorunla Yarın ilgilenebilmem için bunu merge et

--- lvw.js
+++ lvw.js
@@ -1,10 +1,12 @@
 // This page was edited by LinuxUsersLinuxMint
 // Spaces have been removed and the text has been corrected.
-//A Problem Occurring on the Website has been Detected and Fixed by LinuxUsersLinuxMint. If the Problem Still Continues, Please Contact LinuxUsersLinuxMint.
+// A Problem Occurring on the Website has been Detected and Fixed by LinuxUsersLinuxMint. If the Problem Still Continues, Please Contact LinuxUsersLinuxMint.
+
 var clss = document.getElementById("m_cs");
 var menu = document.querySelector("#menu");
 var bodd = document.querySelector("#bodyy");
 var ss = document.querySelector("#s_s");
+
 clss.onclick=function(){
     if(menu.style.width=="60%"){
         menu.style.width=0;
@@ -12,26 +14,28 @@ clss.onclick=function(){
         clss.style.transform="rotate(360deg)";
         clss.style.transition=".4s";
         menu.style.transition=".4s";
-    }else{
-        inp.focus();
-        menu.style.width="60%";
-        clss.innerHTML="⛔";
-        clss.style.transform="";
-    }}
-    bodd.onmouseenter=function(){
-        menu.style.width=0;
-        clss.innerHTML="🔎";
-        clss.style.transform="rotate(360deg)";
-        clss.style.transition=".4s";
-        menu.style.transition=".4s";
-    }
-    var choo = document.querySelector("#s_es");
-    var inp = document.querySelector("#s_bar");
-    var forms =document.querySelector("#form");
+}else{
+    inp.focus();
+    menu.style.width="60%";
+    clss.innerHTML="⛔";
+    clss.style.transform="";
+}}
+
+bodd.onmouseenter=function(){
+    menu.style.width=0;
+    clss.innerHTML="🔎";
+    clss.style.transform="rotate(360deg)";
+    clss.style.transition=".4s";
+    menu.style.transition=".4s";
+}
+var choo = document.querySelector("#s_es");
+var inp = document.querySelector("#s_bar");
+var forms =document.querySelector("#form");
     
 choo.onchange=function(){
     let para = "ile ara";
     let m1 = document.querySelector("#m1");
+    let m2 = document.querySelector("#m2");
     switch(choo.value){
         case "se1":
             m1.style.display="block";
@@ -55,65 +59,70 @@ choo.onchange=function(){
             m2.href= "https://azimli.org/sayfa/hakkimizda/";
             break;
         case "se4":
+            m1.style.display="block";
             inp.placeholder= "TURTC" +" "+ para;
             forms.action= "https://turtc.com/tr/mobil/index.php?page=search/web&search=";
             m1.style.display="none";
             m2.href= "https://turtc.com/hakkimizda/";
             break;
         case "se5":
+            m1.style.display="block";
             inp.placeholder= "YETİTA" +" "+ para;
             forms.action= "https://www.yetita.com/site-ara?kelime=";
             m2.href= "https://www.yetita.com/hakkimizda";
             m1.style.display="none";
             break;
         case "se6":
+            m1.style.display="block";
             inp.placeholder= "BİLİVER" +" "+ para;
             forms.action= "https://www.biliver.com/search?q=";
             m2.href= "https://www.biliver.com/yerli-arama-motoru-biliver.pdf";
             m1.style.display="none";
             break;
         case "se8":
+            m1.style.display="block";
             inp.placeholder= "RASTGELELİK" +" "+ para;
             forms.action= "http://www.rastgelelik.com/search?q=";
             m2.href= "https://www.rastgelelik.com/faq/1-1#3";
             m1.style.display="none";
             break;
         case "se9":
+            m1.style.display="block";
             inp.placeholder= "ARARIM" +" "+ para;
             forms.action= "http://ararim.net/ara?q=";
             m1.style.display="none";
             m2.href= "http://ararim.net/sayfa/ararim-yerli-arama-motoru";
             break;
         case "se10":
+            m1.style.display="block";
             inp.placeholder= "COM" +" "+ para;
             forms.action= "https://com.com.tr/ara/?q=";
             m1.style.display="none";
             m2.style.display="none";
             break;
-       case "se11":
+        case "se11":
+            m1.style.display="block";
             inp.placeholder= "YAANİ" +" "+ para;
             forms.action= "https://www.yaani.com/#q=";
             m1.style.display="none";
             m2.href= "https://www.yaani.com.tr/hakkimizda";
             break;
 }}
-forms.onsubmit=function(){
+forms.onsubmit=function() {
     menu.style.width=0;
     clss.innerHTML="🔎";
     clss.style.transform="rotate(360deg)";
     clss.style.transition=".4s";
     menu.style.transition=".4s";
 }
-
-
-var mods =document.querySelector("#mode");
+var mods=document.querySelector("#mode");
 mods.onclick=function (){
     if(mods.value==1){
         bodd.style.filter="grayscale(1)";
         menu.style.filter="grayscale(1)";
         mods.value=0;
-    }else{
+}   else{
         bodd.style.filter="grayscale(0)";
         menu.style.filter="grayscale(0)";
         mods.value=1;
-    }}
+}}
